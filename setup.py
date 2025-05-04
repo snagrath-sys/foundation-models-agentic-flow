8e941dff4badb89944864511a1cf33fab4a4d76c
Shriya Nagrath <55060695+snagrath-sys@users.noreply.github.com>
2025-05-03 23:23:13 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='foundation_models_agentic_flow',
+    version='0.1',
+    author="Shriya Nagrath",
+    packages=find_packages(),
+    install_requires=[],
+)
